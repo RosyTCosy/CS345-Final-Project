8cc08dc48916bad07177cc19083953cf1bec24d4
Rose Ordway <46855912+RosyTCosy@users.noreply.github.com>
2025-11-20 09:18:04 -0700

    Maps Folder Created Scoreboard data now shows winning team CSV's put in seperate folder

---
+++ maps/role_map.py
@@ -0,0 +1,28 @@
+ROLE_MAP = {
+    "jett": "Duelist",
+    "raze": "Duelist",
+    "reyna": "Duelist",
+    "neon": "Duelist",
+    "yoru": "Duelist",
+    "phoenix": "Duelist",
+
+    "brimstone": "Controller",
+    "omen": "Controller",
+    "astra": "Controller",
+    "viper": "Controller",
+    "harbor": "Controller",
+    "clove": "Controller",
+
+    "sova": "Initiator",
+    "skye": "Initiator",
+    "breach": "Initiator",
+    "kayo": "Initiator",
+    "fade": "Initiator",
+    "gekko": "Initiator",
+
+    "sage": "Sentinel",
+    "killjoy": "Sentinel",
+    "cypher": "Sentinel",
+    "chamber": "Sentinel",
+    "deadlock": "Sentinel",
+}
